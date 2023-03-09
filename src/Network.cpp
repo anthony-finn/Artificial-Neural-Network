2d19c6e6116fb1a6474617d7811ddf8607b4daa4
Anthony Finn <30157407+anthony-finn@users.noreply.github.com>
2023-03-09 12:17:50 -0800

    Updated vectors to pointers

--- src/Network.cpp
+++ src/Network.cpp
@@ -1,5 +1,6 @@
 #include <assert.h>
 #include "../include/Network.hpp"
+#include <iostream>
 
 Network::Network::Network(std::vector<int> t_topology) : m_topology{t_topology}
 { 
@@ -7,18 +8,18 @@ Network::Network::Network(std::vector<int> t_topology) : m_topology{t_topology}
 
     int num_layers = this->m_topology.size();
     this->m_layers.reserve(num_layers);
-    std::vector<Node *> last_layer;
+    std::vector<Node *> *last_layer = nullptr;
     for (int i = num_layers - 1; i >= 0; i--)
     {
         int neuron_count = this->m_topology[i];
-        std::vector<Node *> layer(neuron_count);
+        std::vector<Node *> *layer = new std::vector<Node *>(neuron_count);
         this->m_layers.push_back(layer);
         
         for (int k = 0; k < neuron_count; k++)
         {
             Node *neuron = new Node();
             neuron->connections() = last_layer;
-            layer[k] = neuron;
+            (*layer)[k] = neuron;
         }
 
         last_layer = layer;
@@ -35,12 +36,12 @@ const std::vector<int> &Network::Network::topology() const
     return this->m_topology;
 }
 
-std::vector<std::vector<Network::Node *>> &Network::Network::layers()
+std::vector<std::vector<Network::Node *> *> &Network::Network::layers()
 {
     return this->m_layers;
 }
 
-const std::vector<std::vector<Network::Node *>> &Network::Network::layers() const
+const std::vector<std::vector<Network::Node *> *> &Network::Network::layers() const
 {
     return this->m_layers;
 }
